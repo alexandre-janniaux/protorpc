26a78a25a2556b9d32273ac36b02a550a09dfe47
Tanguy Dubroca <tanguy.dubroca@lse.epita.fr>
2020-11-18 15:32:44 +0100

    sidl: Add IndentedWriter and PrettyPrinter in sidl.utils

---
+++ sidl/sidl/utils.py
@@ -0,0 +1,138 @@
+import io
+from sidl.ast import (
+    Visitor,
+    VariableDeclaration,
+    Symbol,
+    Type,
+    Method,
+    Interface,
+    Namespace,
+)
+
+
+class IndentedWriter:
+    """
+    Helper object for pretty printing.
+    """
+    _is_newline: bool
+    _indentation: int
+
+    def __init__(self, indentation: int = 4) -> None:
+        self._is_newline = False
+        self._indentation = indentation
+        self._level = 0
+        self._stream = io.StringIO()
+
+    def data(self) -> str:
+        return self._stream.getvalue()
+
+    def _write_indent(self) -> None:
+        if self._is_newline:
+            self._stream.write((self._level * self._indentation) * " ")
+            self._is_newline = False
+
+    def _newline(self) -> None:
+        self._stream.write("\n")
+        self._is_newline = True
+
+    def write(self, data: str) -> None:
+        self._write_indent()
+        self._stream.write(data)
+
+    def write_line(self, data: str) -> None:
+        self._write_indent()
+        self._stream.write(data)
+        self._newline()
+
+    def indent(self) -> None:
+        self._level += 1
+
+    def deindent(self) -> None:
+        assert self._level > 0
+        self._level -= 1
+
+
+class PrettyPrinter(Visitor):
+    """
+    Ast pretty printer
+    """
+
+    _writer: IndentedWriter
+
+    def __init__(self, indent=4) -> None:
+        self._writer = IndentedWriter(indent)
+
+    def visit_Type(self, node: Type) -> None:
+        self._writer.write(node.value)
+
+    def visit_Symbol(self, node: Symbol) -> None:
+        self._writer.write(node.value)
+
+    def visit_VariableDeclaration(self, node: VariableDeclaration) -> None:
+        node.type.accept(self)
+        self._writer.write(" ")
+        node.name.accept(self)
+
+    def visit_Method(self, node: Method) -> None:
+        node.name.accept(self)
+        self._writer.write("(")
+
+        for i, arg in enumerate(node.arguments):
+            arg.accept(self)
+
+            if i != len(node.arguments) - 1:
+                self._writer.write(", ")
+
+        self._writer.write(")")
+
+        if node.return_values is not None:
+            self._writer.write(" -> ")
+            self._writer.write("(")
+
+            for i, arg in enumerate(node.return_values):
+                arg.accept(self)
+
+                if i != len(node.return_values) - 1:
+                    self._writer.write(", ")
+
+            self._writer.write(")")
+
+        self._writer.write_line(";")
+
+    def visit_Interface(self, node: Interface) -> None:
+        self._writer.write("interface ")
+        node.name.accept(self)
+
+        if len(node.methods) == 0:
+            self._writer.write_line(" {}")
+            return
+
+        self._writer.write_line(" {")
+        self._writer.indent()
+
+        for meth in node.methods:
+            meth.accept(self)
+
+        self._writer.deindent()
+        self._writer.write_line("}")
+
+    def visit_Namespace(self, node: Namespace) -> None:
+        self._writer.write("namespace ")
+        node.name.accept(self)
+
+        if len(node.elements) == 0:
+            self._writer.write_line(" {}")
+            return
+
+        self._writer.write_line(" {")
+        self._writer.indent()
+
+        for elem in node.elements:
+            elem.accept(self)
+
+        self._writer.deindent()
+        self._writer.write_line("}")
+
+    @property
+    def data(self) -> str:
+        return self._writer.data()
--- sidl/test.py
+++ sidl/test.py
@@ -1,94 +1,9 @@
 from sidl.ast import Visitor, VariableDeclaration, Symbol, Type, Method, Interface, Namespace
-
-
-class PrettyPrinter(Visitor):
-    def __init__(self, indent=4) -> None:
-        self.indent_level = 0
-        self.indent = indent
-        self.data = ""
-
-    def append(self, s) -> None:
-        self.data += s
-
-    def newline(self) -> None:
-        self.data += "\n"
-        self.data += " " * (self.indent_level * self.indent)
-
-    def visit_Type(self, node: Type) -> None:
-        self.append(node.value)
-
-    def visit_Symbol(self, node: Symbol) -> None:
-        self.append(node.value)
-
-    def visit_VariableDeclaration(self, node: VariableDeclaration) -> None:
-        node.type.accept(self)
-        self.append(" ")
-        node.name.accept(self)
-
-    def visit_Method(self, node: Method) -> None:
-        node.name.accept(self)
-        self.append("(")
-
-        for i, arg in enumerate(node.arguments):
-            arg.accept(self)
-
-            if i != len(node.arguments) - 1:
-                self.append(", ")
-
-        self.append(")")
-
-        if node.return_values is not None:
-            self.append(" -> ")
-            self.append("(")
-
-            for i, arg in enumerate(node.return_values):
-                arg.accept(self)
-
-                if i != len(node.return_values) - 1:
-                    self.append(", ")
-
-            self.append(")")
-
-        self.append(";")
-
-    def visit_Interface(self, node: Interface) -> None:
-        self.append("interface ")
-        node.name.accept(self)
-        self.append(" {")
-
-        if len(node.methods) > 0:
-            self.indent_level += 1
-
-        for meth in node.methods:
-            self.newline()
-            meth.accept(self)
-
-        if len(node.methods) > 0:
-            self.indent_level -= 1
-            self.newline()
-
-        self.append("}")
-
-    def visit_Namespace(self, node: Namespace) -> None:
-        self.append("namespace ")
-        node.name.accept(self)
-        self.append(" {")
-
-        if len(node.elements) > 0:
-            self.indent_level += 1
-
-        for elem in node.elements:
-            self.newline()
-            elem.accept(self)
-
-        if len(node.elements) > 0:
-            self.indent_level -= 1
-            self.newline()
-
-        self.append("}")
+from sidl.utils import PrettyPrinter
 
 
 def main():
+    namespace = Namespace(Symbol("vlc"))
     vlc_stream = Interface(Symbol("VlcStream"))
 
     offset_method = Method(Symbol("offset"))
@@ -97,8 +12,10 @@ def main():
 
     vlc_stream.add_method(offset_method)
 
+    namespace.add_element(vlc_stream)
+
     pprinter = PrettyPrinter()
-    pprinter.visit(vlc_stream)
+    pprinter.visit(namespace)
 
     print(pprinter.data)
 

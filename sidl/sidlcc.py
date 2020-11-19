d4247d088efc6e2ddff1586b941f45e74186e966
Tanguy Dubroca <tanguy.dubroca@lse.epita.fr>
2020-11-19 15:25:54 +0100

    sidl: Add example error message on parsing error

--- sidl/sidlcc.py
+++ sidl/sidlcc.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import argparse
+from sidl.lexer import Lexer
+from sidl.parser import Parser, ParsingException
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="protorpc Small IDL compiler")
@@ -16,3 +18,20 @@ if __name__ == "__main__":
     parser.add_argument("idl_file", help="input idl file")
 
     args = parser.parse_args()
+
+    # XXX: Debug code
+    data = open(args.idl_file, "r").read()
+    lines = data.split("\n")
+    lex = Lexer(data)
+    p = Parser(lex)
+
+    try:
+        root = p.parse()
+    except ParsingException as err:
+        start = max(err.line - 3, 0)
+
+        for line_idx in range(start, err.line):
+            print(lines[line_idx])
+
+        print(" "*(err.col - 1) + "^")
+        print(f"{args.idl_file}:{err.line}:{err.col}: {err.message}")

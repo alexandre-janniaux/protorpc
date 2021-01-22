b6533709103bd0943651f618af169cc55def42a4
Tanguy Dubroca <tanguy.dubroca@lse.epita.fr>
2021-01-22 16:55:47 +0100

    libcprotorpc: Macro madness

--- libcprotorpc/include/cprotorpc/structures.h
+++ libcprotorpc/include/cprotorpc/structures.h
@@ -10,38 +10,10 @@
 extern "C" {
 #endif
 
-/*
- * Generic vector of elements (used for user defined datatypes)
- */
-typedef struct sidl_generic_vector_t
-{
-    size_t size;
-    size_t capacity;
-    void** elements;
-} sidl_generic_vector_t;
+// Vector declaration
+#define SIDL_VSNAME(SIDL_TYPE) sidl_##SIDL_TYPE##_vector
 
-int sidl_generic_vector_init(sidl_generic_vector_t* vec);
-void sidl_generic_vector_destroy(sidl_generic_vector_t* vec);
-
-/*
- * Frees inner content of the passed vector.
- */
-void sidl_generic_vector_destroy(sidl_generic_vector_t* vec);
-int sidl_generic_vector_append(sidl_generic_vector_t* vec, void* element);
-
-inline static void* sidl_generic_vector_get(sidl_generic_vector_t* vec, size_t index)
-{
-    assert(index < vec->size && "Vector access out of bounds");
-    return vec->elements[index];
-}
-
-/*
- * Using generic vectors for small copiable datatypes is a waste.
- */
-
-#define SIDL_VSNAME(SIDL_TYPE) sidl_##SIDL_TYPE##_vector_t
-
-#define SIDL_POD_VEC_STRUCT(SIDL_TYPE, C_TYPE) \
+#define SIDL_VEC_STRUCT(SIDL_TYPE, C_TYPE) \
 typedef struct SIDL_VSNAME(SIDL_TYPE) \
 { \
     size_t size; \
@@ -49,9 +21,9 @@ typedef struct SIDL_VSNAME(SIDL_TYPE) \
     C_TYPE* elements; \
 } SIDL_VSNAME(SIDL_TYPE);
 
-#define SIDL_POD_VEC_DECL(SIDL_TYPE, C_TYPE) \
+#define SIDL_VEC_DECL(SIDL_TYPE, C_TYPE) \
 int sidl_##SIDL_TYPE##_vector_init(SIDL_VSNAME(SIDL_TYPE)* v); \
-int sidl_##SIDL_TYPE##_vector_from(SIDL_VSNAME(SIDL_TYPE)* v, C_TYPE* data, size_t count); \
+int sidl_##SIDL_TYPE##_vector__from(SIDL_VSNAME(SIDL_TYPE)* v, C_TYPE* data, size_t count); \
 int sidl_##SIDL_TYPE##_vector_append(SIDL_VSNAME(SIDL_TYPE)* v, C_TYPE value); \
 void sidl_##SIDL_TYPE##_vector_destroy(SIDL_VSNAME(SIDL_TYPE)* v); \
 \
@@ -60,31 +32,112 @@ static inline C_TYPE sidl_##SIDL_TYPE##_vector_get(SIDL_VSNAME(SIDL_TYPE)* v, si
     return v->elements[index]; \
 }
 
-XM_SIDL_TYPES(SIDL_POD_VEC_STRUCT)
-XM_SIDL_TYPES(SIDL_POD_VEC_DECL)
+#define SIDL_DECLARE_VECTOR_PROTO(SIDL_TYPE, C_TYPE) \
+    SIDL_VEC_STRUCT(SIDL_TYPE, C_TYPE) \
+    SIDL_VEC_DECL(SIDL_TYPE, C_TYPE)
 
-#undef SIDL_VSNAME
-#undef SIDL_POD_VEC_STRUCT
-#undef SIDL_POD_VEC_DECL
+XM_SIDL_TYPES(SIDL_DECLARE_VECTOR_PROTO)
+
+// Special case for the string type
+SIDL_DECLARE_VECTOR_PROTO(string, char*)
+
+// Vector implementation
+#define SIDL_VECTOR_CAPACITY (10)
+
+#define SIDL_VEC_INIT_IMPL(SIDL_TYPE, C_TYPE) \
+int sidl_##SIDL_TYPE##_vector_init(SIDL_VSNAME(SIDL_TYPE)* v) { \
+    v->size = 0; \
+    v->capacity = SIDL_VECTOR_CAPACITY; \
+    v->elements = malloc(sizeof(C_TYPE) * v->capacity); \
+    \
+    if (!v->elements) \
+        return -1; \
+    \
+    return 0; \
+}
+
+#define SIDL_VEC_INIT_FROM_IMPL(SIDL_TYPE, C_TYPE) \
+int sidl_##SIDL_TYPE##_vector_from(SIDL_VSNAME(SIDL_TYPE)* v, C_TYPE* data, size_t count) { \
+    v->size = count; \
+    v->capacity = count; \
+    v->elements = malloc(sizeof(C_TYPE) * v->capacity); \
+    \
+    if (!v->elements) \
+        return -1; \
+    \
+    memcpy(v->elements, data, count * sizeof(C_TYPE)); \
+    \
+    return 0; \
+}
+
+#define SIDL_VEC_APPEND_IMPL(SIDL_TYPE, C_TYPE) \
+int sidl_##SIDL_TYPE##_vector_append(SIDL_VSNAME(SIDL_TYPE)* v, C_TYPE value) { \
+    if (v->size >= v->capacity) \
+    { \
+        size_t new_capacity = v->capacity * 2; \
+        C_TYPE* new_array = realloc(v->elements, new_capacity * sizeof(C_TYPE)); \
+        \
+        if (!new_array) \
+            return -1; \
+        \
+        v->capacity = new_capacity; \
+        v->elements = new_array; \
+    } \
+    \
+    v->elements[v->size++] = value; \
+    \
+    return 0; \
+}
+
+#define SIDL_VEC_DESTROY_IMPL(SIDL_TYPE, C_TYPE) \
+void sidl_##SIDL_TYPE##_vector_destroy(SIDL_VSNAME(SIDL_TYPE)* v) { \
+    free(v->elements); \
+} \
+
+#define SIDL_DECLARE_VECTOR_IMPL(SIDL_TYPE, C_TYPE) \
+        SIDL_VEC_INIT_IMPL(SIDL_TYPE, C_TYPE) \
+        SIDL_VEC_INIT_FROM_IMPL(SIDL_TYPE, C_TYPE) \
+        SIDL_VEC_APPEND_IMPL(SIDL_TYPE, C_TYPE)
 
 /*
  * Optional type. Object is present if element ptr is non null
  */
-typedef struct sidl_optional_t
-{
-    void* element;
-} sidl_optional_t;
 
-inline static int sidl_optional_empty(sidl_optional_t* opt)
-{
-    return opt->element != NULL;
-}
+#define SIDL_OSNAME(SIDL_TYPE) sidl_##SIDL_TYPE##_optional
+#define SIDL_OPT_STRUCT(SIDL_TYPE, C_TYPE) \
+typedef struct sidl_##SIDL_TYPE##_optional \
+{ \
+    uint8_t present; \
+    C_TYPE element; \
+} sidl_##SIDL_TYPE##_optional; \
 
-inline static void* sidl_optional_data(sidl_optional_t* opt)
-{
-    assert(!sidl_optional_empty(opt) && "Tried to get element from empty optional");
-    return opt->element;
-}
+#define SIDL_OPT_DECL(SIDL_TYPE, C_TYPE) \
+static inline int sidl_##SIDL_TYPE##_optional_empty(SIDL_OSNAME(SIDL_TYPE)* opt) \
+{ \
+    return !opt->present; \
+} \
+\
+inline static C_TYPE* sidl_##SIDL_TYPE##_optional_data(SIDL_OSNAME(SIDL_TYPE)* opt) \
+{ \
+    assert(!sidl_##SIDL_TYPE##_optional_empty(opt) && "Tried to get element from empty optional"); \
+    return &opt->element; \
+} \
+\
+void sidl_##SIDL_TYPE##_optional_destroy(SIDL_OSNAME(SIDL_TYPE)* opt); \
+
+#define SIDL_DECLARE_OPTIONAL_PROTO(SIDL_TYPE, C_TYPE) \
+        SIDL_OPT_STRUCT(SIDL_TYPE, C_TYPE) \
+        SIDL_OPT_DECL(SIDL_TYPE, C_TYPE)
+
+XM_SIDL_TYPES(SIDL_DECLARE_OPTIONAL_PROTO)
+SIDL_DECLARE_OPTIONAL_PROTO(string, char*)
+
+#define SIDL_OPT_DESTROY_IMPL(SIDL_TYPE, C_TYPE) \
+void sidl_##SIDL_TYPE##_optional_destroy(SIDL_OSNAME(SIDL_TYPE)* opt) \
+{}
+
+#define SIDL_DECLARE_OPTIONAL_IMPL(SIDL_TYPE, C_TYPE) \
+        SIDL_OPT_DESTROY_IMPL(SIDL_TYPE, C_TYPE)
 
 #ifdef __cplusplus
 }
--- libcprotorpc/src/structures.c
+++ libcprotorpc/src/structures.c
@@ -2,104 +2,25 @@
 #include <string.h>
 #include "cprotorpc/structures.h"
 
-#define SIDL_VECTOR_CAPACITY (10)
+// Vectors
+XM_SIDL_TYPES(SIDL_DECLARE_VECTOR_IMPL)
+XM_SIDL_TYPES(SIDL_VEC_DESTROY_IMPL)
 
-int sidl_generic_generic_vector_new(sidl_generic_vector_t* vec)
+// Special case for the string type
+SIDL_DECLARE_VECTOR_IMPL(string, char*)
+
+void sidl_string_vector_destroy(SIDL_VSNAME(string)* v)
 {
-    vec->size = 0;
-    vec->capacity = SIDL_VECTOR_CAPACITY;
-    vec->elements = calloc(vec->capacity, sizeof(void*));
+    for (size_t i = 0; i < v->size; i++)
+        free(v->elements[i]);
 
-    if (!vec->elements)
-        return -1;
-
-    return 0;
+    free(v->elements);
 }
 
-void sidl_generic_vector_destroy(sidl_generic_vector_t* vec)
+// Optionals
+XM_SIDL_TYPES(SIDL_DECLARE_OPTIONAL_IMPL)
+
+void sidl_string_optional_destroy(SIDL_OSNAME(string)* v)
 {
-    free(vec->elements);
+    free(v->element);
 }
-
-int sidl_generic_vector_append(sidl_generic_vector_t* vec, void* element)
-{
-    if (vec->size >= vec->capacity)
-    {
-        size_t new_capacity = vec->capacity * 2;
-        void** new_array = realloc(vec->elements, new_capacity * sizeof(void*));
-
-        if (!new_array)
-            return -1;
-
-        vec->capacity = new_capacity;
-        vec->elements = new_array;
-    }
-
-    vec->elements[vec->size++] = element;
-
-    return 0;
-}
-
-// SIDL specific vectors
-#define SIDL_VSNAME(SIDL_TYPE) sidl_##SIDL_TYPE##_vector_t
-
-#define SIDL_VEC_INIT(SIDL_TYPE, C_TYPE) \
-int sidl_##SIDL_TYPE##_vector_init(SIDL_VSNAME(SIDL_TYPE)* v) { \
-    v->size = 0; \
-    v->capacity = SIDL_VECTOR_CAPACITY; \
-    v->elements = malloc(sizeof(C_TYPE) * v->capacity); \
-    \
-    if (!v->elements) \
-        return -1; \
-    \
-    return 0; \
-}
-
-#define SIDL_VEC_INIT_FROM(SIDL_TYPE, C_TYPE) \
-int sidl_##SIDL_TYPE##_vector_from(SIDL_VSNAME(SIDL_TYPE)* v, C_TYPE* data, size_t count) { \
-    v->size = count; \
-    v->capacity = count; \
-    v->elements = malloc(sizeof(C_TYPE) * v->capacity); \
-    \
-    if (!v->elements) \
-        return -1; \
-    \
-    memcpy(v->elements, data, count * sizeof(C_TYPE)); \
-    \
-    return 0; \
-}
-
-#define SIDL_VEC_APPEND(SIDL_TYPE, C_TYPE) \
-int sidl_##SIDL_TYPE##_vector_append(SIDL_VSNAME(SIDL_TYPE)* v, C_TYPE value) { \
-    if (v->size >= v->capacity) \
-    { \
-        size_t new_capacity = v->capacity * 2; \
-        C_TYPE* new_array = realloc(v->elements, new_capacity * sizeof(C_TYPE)); \
-        \
-        if (!new_array) \
-            return -1; \
-        \
-        v->capacity = new_capacity; \
-        v->elements = new_array; \
-    } \
-    \
-    v->elements[v->size++] = value; \
-    \
-    return 0; \
-}
-
-#define SIDL_VEC_DESTROY(SIDL_TYPE, C_TYPE) \
-void sidl_##SIDL_TYPE##_vector_destroy(SIDL_VSNAME(SIDL_TYPE)* v) { \
-    free(v->elements); \
-} \
-
-XM_SIDL_TYPES(SIDL_VEC_INIT)
-XM_SIDL_TYPES(SIDL_VEC_INIT_FROM)
-XM_SIDL_TYPES(SIDL_VEC_APPEND)
-XM_SIDL_TYPES(SIDL_VEC_DESTROY)
-
-#undef SIDL_VSNAME
-#undef SIDL_VEC_DESTROY
-#undef SIDL_VEC_APPEND
-#undef SIDL_VEC_INIT_FROM
-#undef SIDL_VEC_INIT
